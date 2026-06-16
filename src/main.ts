07d042a05473c817e1eaa74a2b02326b099d0945
EduLago <eduardolagonunes@gmail.com>
2026-06-16 13:25:53 -0300

    FIX: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://multimensional-chess.netlify.app/'],
+    origin: [
+      'http://localhost:5173',
+      'https://multimensional-chess.netlify.app',
+      'https://www.multimensional-chess.netlify.app'
+    ],
     credentials: true,
   });
 
